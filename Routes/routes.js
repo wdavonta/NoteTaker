a7acce3fac91f70f63dbf445b6cda60eb8e733d1
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-15 15:47:55 -0500

    add server and route js, added const to start the paget and app.

---
+++ Routes/routes.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const pathe = require('path');
+
+
+// this page you will do the app.get
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require ('fs');
+const path = require ('path');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
