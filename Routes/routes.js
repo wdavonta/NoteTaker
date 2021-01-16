86d8ea236e681e76a25ce89b699f3dcfb7b28cf2
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-16 14:25:10 -0500

    added api specific note

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,5 +1,38 @@
 const fs = require('fs');
-const pathe = require('path');
+const path = require('path');
 
 
 // this page you will do the app.get
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+
+});
+
+
+// api routes
+app.get('/api/notes/:id', (req, res) => {
+    res.json(notes[req.params.id]);
+});
+
+
+app.post('/api/notes', (req, res) => {
+    const newData = req.body;
+    notes.push('data');
+
+    return res.json(newData);
+
+
+}) ;
+
+
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+require('./Routes/routes');
+
+
 
 
 app.listen(PORT, () => {
