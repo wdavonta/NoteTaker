31f4e16c7bcec8f5e66305446c5b3be5f944bf5d
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-17 16:20:03 -0500

    update

--- Routes/routes.js
+++ Routes/routes.js
@@ -10,10 +10,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-//   });
-
   
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
